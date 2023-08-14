b355b7e0e2c9024597854623b0a8d6fd5da2d71e
Subhransu Panda <subhransupanda2000@gmail.com>
2023-08-14 18:34:35 +0530

    Constructer01

---
+++ src/main/java/Constructer.java
@@ -0,0 +1,16 @@
+public class Constructer {
+    int a;String b;
+    //Constructer()
+    //{
+//a=0;b="akash";
+    //}
+    void show()
+    {
+        System.out.println(a + b);
+    }
+    public static void main (String [] args)
+    {
+        Constructer c=new Constructer();
+        c.show();
+    }
+}
